f06fa229efae9527936adf03b76bd479fb936229
divyancod <dysu135@gmail.com>
2021-01-24 14:23:43 +0530

    site upgrade

---
+++ script.js
@@ -0,0 +1,8 @@
+const titleText = document.querySelector(".blink");
+var typed = new Typed('.typing', {
+    strings: ["Android Developer.", "Java SE Developer.","Confused Learner.","HTML.CSS.JS"],
+    loop: true,
+    typeSpeed: 80,
+    backSpeed: 40
+  });
+  AOS.init();
